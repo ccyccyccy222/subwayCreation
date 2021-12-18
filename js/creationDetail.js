81c69cb09ff748442d7b129a9179f6ea8fc77aca
ccyccyccy222 <1092098326@qq.com>
2021-12-18 12:26:55 +0800

    完成了上传功能

--- js/creationDetail.js
+++ js/creationDetail.js
@@ -24,8 +24,40 @@ let creation={
 
 // 页面刚加载时调用的方法
 initPage=()=>{
-    creation=JSON.stringify(creation);
-    localStorage.setItem("creationDetail",creation)
+    // creation=JSON.stringify(creation);
+    // sessionStorage.setItem("creationDetail",creation)
+    creation=sessionStorage.getItem("creationDetail");
+    creation=JSON.parse(creation);
+
+
+    //初始化评论内容
+    initComment();
+}
+
+initComment=()=>{
+    for(let i=0;i<creation.comment.length;i++){
+        let item=creation.comment[i];
+        let username=item.username;
+        let avaterUrl=item.avaterUrl;
+        let content=item.content;
+
+        // let ul=document.getElementById("commentUL");
+        let li=document.getElementById("modelLi");
+        let cloneNode=li.cloneNode(true);
+        cloneNode.style.display="block";
+        let image=cloneNode.getElementsByTagName("img")[0];
+        image.setAttribute("src",avaterUrl);
+        let h4=cloneNode.getElementsByTagName("h4")[0];
+        h4.innerHTML=username;
+        let p=cloneNode.getElementsByTagName("p")[0];
+        p.innerHTML=content;
+        console.log(cloneNode);
+        li.parentNode.appendChild(cloneNode);
+        let hr=document.createElement("hr");
+        li.parentNode.appendChild(hr);
+    }
+
+
 }
 
 // 点击小图标调用的方法
@@ -46,6 +78,19 @@ changeColor=(id)=>{
     let text=p.innerHTML;
     console.log(text);
     p.innerHTML=String(Number(text) + 1);
+}
+
+// 发表评论的方法
+submit=()=>{
+    let content=document.getElementById("message-text").value;
+    console.log(content);
+
+    let item={...creation.comment[0]};
+    item.content=content;
+    creation.comment.push(item);
+    creation=JSON.stringify(creation);
+    sessionStorage.setItem("creationDetail",creation);
+    location.reload();
 }
 
 
